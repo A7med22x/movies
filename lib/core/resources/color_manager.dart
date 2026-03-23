6734b0ebf2c34c4784940ca5e8ba532837540fe7
Ahmed AbdAlmoniem <ahmedabdelmenem666@gmail.com>
2026-03-23 11:32:59 +0200

    Create core Folder

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xFFF6BD00);
+  static const Color background = Color(0xFF121312);
+  static const Color lightBlack = Color(0xFF282A28);
+  static const Color yellow = Color(0xFFFFBB3B);
+  static const Color green = Color(0xff57AA53);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color error = Color(0xffe82626);
+}
